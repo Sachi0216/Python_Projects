4e6b498882a378ddf3970c1ed8a7316c403590c7
Sachi0216 <83989398+Sachi0216@users.noreply.github.com>
2022-02-10 14:31:22 -0600

    Add files via upload

---
+++ Python_Projects/python_projects/test8.py
@@ -0,0 +1,48 @@
+
+import sqlite3
+
+
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf','myPhoto.jpg')
+
+conn = sqlite3.connect('test.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fname TEXT, \
+        col_lname TEXT, \
+        col_email TEXT \
+        )")
+    conn.commit()
+conn.close()
+
+
+
+conn = sqlite3.connect('test.db')
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ('Elijah', 'Davis', 'edavis@gmail.com'))
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ('Anakin', 'Skywalker', 'skywalker1@gmail.com'))
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ('Luke', 'Skywalker', 'skywalker2@gmail.com'))
+    conn.commit()
+conn.close()
+
+
+
+conn = sqlite3.connect('test.db')
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT col_fname,col_lname,col_email FROM tbl_persons WHERE col_fname = 'Elijah'")
+    varPerson = cur.fetchall()
+    for item in varPerson:
+        msg = "First Name: {}\nLast Name: {}\nEmail: {}".format(item[0],item[1],item[2])
+    print(msg)
